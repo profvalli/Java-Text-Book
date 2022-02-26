6955bd6202d3ece22bbb76cd7159410a3038b35d
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 15:09:13 +0530

    Create program3.9-asciiChars.java

---
+++ program3.9-asciiChars.java
@@ -0,0 +1,50 @@
+//Program 3.9: To list all ASCII chars
+public class asciiChars
+{
+	public static void main(String args[])
+	{
+		for(char c=0; c<128; c++)
+		
+			if(c!=26)
+			System.out.println("vale:"+(int)c+"character:"+c););
+				
+	}
+}
+/*
+Output:
+....
+value:33  character:!
+value:34  character:"
+value:35  character:#
+value:36  character:$
+value:37  character:%
+value:38  character:&
+value:39  character:'
+value:40  character:(
+value:41  character:)
+value:42  character:*
+value:43  character:+
+value:44  character:,
+value:45  character:-
+value:46  character:.
+value:47  character:/
+value:48  character:0
+value:49  character:1
+value:50  character:2
+value:51  character:3
+value:52  character:4
+value:53  character:5
+value:54  character:6
+value:55  character:7
+value:56  character:8
+value:57  character:9
+value:58  character::
+value:59  character:;
+value:60  character:<
+value:61  character:=
+value:62  character:>
+value:63  character:?
+value:64  character:@
+value:65  character:A
+.....
+*/
