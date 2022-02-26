428e7f8bbceb83d56ee88bf5b952bd1510b8d211
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 15:30:01 +0530

    Create program3.11-breakStmt.java

---
+++ program3.11-breakStmt.java
@@ -0,0 +1,30 @@
+//Program 3.11: Using break statement
+
+class breakStmt
+{
+	public static void main(String args[])
+	{
+		boolean t=true;
+		first:
+		{
+			//name of the first block
+			second:
+			{
+				//name of the second block
+				third:
+				{
+					//name of the third block
+					System.out.println("this won't execute");					
+				}
+				System.out.println("This will also not work");
+			}
+			System.out.println("THis is after second block");
+		}
+	}
+}
+
+/* Output:
+this won't execute
+This will also not work
+THis is after second block
+*/
