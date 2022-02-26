980560354f74f86152a532da68e287b5d98cf251
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 15:01:28 +0530

    Create program3.8-factorial.java

---
+++ program3.8-factorial.java
@@ -0,0 +1,15 @@
+//Program 3.8- To find factorial of a given number
+class factorial
+{
+	public static void main(String args[])
+	{
+		int n=5, fact=1,i;
+		for (i=1;i<=5;i++)
+			fact=fact*i;
+		System.out.println("fact="+fact);
+	}
+
+}
+/* Output:
+fact=120
+*/
