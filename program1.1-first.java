ee9997277d0d1e2dfe17d450709f79eefb409577
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 13:24:34 +0530

    Create program1.1-first.java

---
+++ program1.1-first.java
@@ -0,0 +1,8 @@
+//Program 1.1: Running the first java program first.java
+class first{
+public static void main(String args[]){
+	System.out.println("my first java program");
+}
+}
+// output:
+//my first java program
