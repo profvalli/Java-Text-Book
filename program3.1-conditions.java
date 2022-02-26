5d8e4348e85194b1180d5cf93afb128d7eab968c
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 12:49:10 +0530

    Create program3.1-conditions.java

---
+++ program3.1-conditions.java
@@ -0,0 +1,76 @@
+// Program 3.1 : Demonstrating conditions. Print the day of the week depending on the value of i.
+public class conditions
+{
+  public static void main (String args[])
+  {
+    int i;
+      i = 4;
+    if (i == 0)
+      {
+	System.out.println ("it is Sunday");
+      }
+    else if (i == 1)
+      {
+	System.out.println ("it is Monday");
+      }
+    else if (i == 2)
+      {
+	System.out.println ("it is Tuesday");
+      }
+    else if (i == 3)
+      {
+	System.out.println ("it is Wednesday");
+      }
+    else if (i == 4)
+      {
+	System.out.println ("it is Thursday");
+      }
+    else if (i == 5)
+      {
+	System.out.println ("it is Friday");
+      }
+    else if (i == 6)
+      {
+	System.out.println ("it is Satuday");
+      }
+  }
+}
+// public class Main
+{
+  public static void main (String args[])
+  {
+    int i;
+      i = 4;
+    if (i == 0)
+      {
+	System.out.println ("it is Sunday");
+      }
+    else if (i == 1)
+      {
+	System.out.println ("it is Monday");
+      }
+    else if (i == 2)
+      {
+	System.out.println ("it is Tuesday");
+      }
+    else if (i == 3)
+      {
+	System.out.println ("it is Wednesday");
+      }
+    else if (i == 4)
+      {
+	System.out.println ("it is Thursday");
+      }
+    else if (i == 5)
+      {
+	System.out.println ("it is Friday");
+      }
+    else if (i == 6)
+      {
+	System.out.println ("it is Satuday");
+      }
+  }
+}
+
+//Output:
+//it is Thursday
