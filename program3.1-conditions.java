a68ca087a860406771e7e9f89a1bc1e72e2412c1
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 12:57:56 +0530

    Update program3.1-conditions.java

--- program3.1-conditions.java
+++ program3.1-conditions.java
@@ -35,42 +35,5 @@ public class conditions
       }
   }
 }
-// public class Main
-{
-  public static void main (String args[])
-  {
-    int i;
-      i = 4;
-    if (i == 0)
-      {
-	System.out.println ("it is Sunday");
-      }
-    else if (i == 1)
-      {
-	System.out.println ("it is Monday");
-      }
-    else if (i == 2)
-      {
-	System.out.println ("it is Tuesday");
-      }
-    else if (i == 3)
-      {
-	System.out.println ("it is Wednesday");
-      }
-    else if (i == 4)
-      {
-	System.out.println ("it is Thursday");
-      }
-    else if (i == 5)
-      {
-	System.out.println ("it is Friday");
-      }
-    else if (i == 6)
-      {
-	System.out.println ("it is Satuday");
-      }
-  }
-}
-
 //Output:
 //it is Thursday
