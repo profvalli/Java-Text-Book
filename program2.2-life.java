a9be306851e0fcc318985151912a259a45b8a14a
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-26 13:39:39 +0530

    Create program2.2-life.java

---
+++ program2.2-life.java
@@ -0,0 +1,22 @@
+class life
+{
+	public static void  main(String args[])
+	{
+	int i,j;
+	for(j=0; j<3;j++)
+	{
+		i=1;
+		System.out.println("i="+i);
+		i=100;
+		System.out.println("i="+i);
+	}
+	}
+}
+/* Output:
+i=1
+i=100
+i=1
+i=100
+i=1
+i=100
+*/
