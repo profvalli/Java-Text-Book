b0b79169d2412730cfd42ab1d891081908b39850
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2022-02-27 20:25:35 +0530

    Create program4.1-findAverage.java

---
+++ program4.1-findAverage.java
@@ -0,0 +1,20 @@
+//Program 4.1: Finding average of given array of numbers
+public class findAverage
+{
+  public static void main (String[]args)
+  {
+    int a[] = { 76, 98, 79, 78, 77, 84, 85, 86, 89, 90 };
+    float result = 0, avg=0;
+    for (int i = 0; i < 9; i++)
+      {
+	result = result + a[i];
+	avg = result / 10;
+      }
+    System.out.println ("avg is " + avg);
+  }
+}
+
+/*
+Output:
+avg is 75.2
+*/
