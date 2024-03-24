361b0b637f89113d8b9a8124a506bccdfdfd31fe
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-24 09:08:44 +0530

    Create program2.4-stringOp.java

---
+++ program2.4-stringOp.java
@@ -0,0 +1,18 @@
+//stringOp.java
+public class stringOp
+{
+public static void main(String[] args) {
+System.out.println("Hello World");
+int a=0,b=1,c=2;
+
+
+String s="a,b,c";
+
+
+System.out.println(s+a+b+c);
+}
+}
+
+//Output:
+
+a,b,c012
