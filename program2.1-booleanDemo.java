b5a14f9568378d9bcd89c7a7bdbd18bb4883dca2
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-24 07:49:38 +0530

    Create program2.1-booleanDemo.java

---
+++ program2.1-booleanDemo.java
@@ -0,0 +1,13 @@
+public class booleanDemo
+{
+public static void main(String[] args) {
+System.out.println("Hello World");
+boolean var1 = true;
+boolean var2 = false;
+System.out.println(var1); // Outputs true
+System.out.println(var2); // Outputs false } 
+} 
+
+//Output: 
+//true 
+//false
