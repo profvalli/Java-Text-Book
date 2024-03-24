cc14a03caba092d7706bf51d29bc77f9dd5f0bf2
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-25 01:07:15 +0530

    Create program5.2-reversing.java

---
+++ program5.2-reversing.java
@@ -0,0 +1,30 @@
+//Program 5.2 Program to reverse a string using a custom defined function
+//reversing.java
+//click here for code
+import java.lang.*;
+class rev{
+public static String reverse(String s){
+int i,length;
+length=s.length();
+System.out.println(" iam here");
+StringBuffer revst = new StringBuffer(length);
+for(i=length-1;i>=0;i--)
+{
+System.out.println(i);
+revst.append(s.charAt(i));
+}
+return revst.toString();
+}
+}
+
+class reversing{
+public static void main(String args[])
+{
+StringBuffer s = new StringBuffer("I am getting reversed");
+System.out.println(rev.reverse(s.toString()));
+}
+}
+
+/*Output:
+desrever gnitteg ma I
+*/
