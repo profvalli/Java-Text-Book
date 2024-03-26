510aafa1f02088e82d114b3fd73018ca874883ec
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-26 14:52:05 +0530

    Update program6.1-studClass.java

--- program6.1-studClass.java
+++ program6.1-studClass.java
@@ -1,34 +1,18 @@
 //studClass.java
 class student
-
 {
-
-int rollno;
-
-float marks1, marks2;
-
+  int rollno;
+  float marks1, marks2;
 }
-
 class studClass{
-
 public static void main(String args[])
-
 {
-
-student student1=new student();
-
-student student2=new student();
-
-float total;
-
-student1.marks1=82;
-
-student2.marks2=79;
-
-total=student1.marks1+student2.marks2;
-
-System.out.println("total marks="+total);
-
+  student student1=new student();
+  student student2=new student();
+  float total;
+  student1.marks1=82;
+  student2.marks2=79;
+  total=student1.marks1+student2.marks2;
+  System.out.println("total marks="+total);
 }
-
 }
