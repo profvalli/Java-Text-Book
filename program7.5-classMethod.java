488596b40f731b3c0711e8f2073e4c339aaecf80
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-28 05:15:33 +0530

    Update program7.5-classMethod.java

--- program7.5-classMethod.java
+++ program7.5-classMethod.java
@@ -1,31 +1,18 @@
 //Program 7.5 Demonstration of class method.
-
 //classMethod.java
-
 class xyz{
-
 public static void xyzmethod()
-
 {
-
-System.out.println("in xyz method");
-
+  System.out.println("in xyz method");
 }
-
 }
 
 public class classMethod{
-
 public static void main(String args[])
-
 {
-
 //calling class method.
-
-xyz.xyzmethod();
-
+ xyz.xyzmethod();
 }
-
 }
 /*
 Output:
