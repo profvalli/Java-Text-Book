329fedf2a6290004e53f38dcc2605ffec0d2c6f4
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-28 04:55:48 +0530

    Create passObj.java

---
+++ passObj.java
@@ -0,0 +1,46 @@
+//Program 7.3 Program to pass objects to methods.
+//passObj.java
+class test{
+
+int a,b;
+
+test(int i,int j){
+
+a=i;
+
+b=j;
+
+}
+
+boolean equals(test ob){
+
+if(ob.a==a&&ob.b==b) return true;
+
+else return false;
+
+}
+
+}
+
+class passObj{
+
+public static void main(String args[]){
+
+test ob1=new test(100,22);
+
+test ob2=new test(100,22);
+
+test ob3=new test(-1,-1);
+
+System.out.println("ob1==ob2:"+ob1.equals(ob2));
+
+System.out.println("ob1==ob3:"+ob1.equals(ob3));
+
+}
+
+}
+/*
+Output:
+ob1==ob2:true
+ob1==ob3:false
+*/
