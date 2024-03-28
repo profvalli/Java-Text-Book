113976d0a9e78a281c6fccb35e57a018686efd27
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-28 15:02:08 +0530

    Create program8.1-superDemo.java

---
+++ program8.1-superDemo.java
@@ -0,0 +1,42 @@
+Program 8.1 Program to demonstrate super and child classes 
+//superDemo.java
+
+class candidate
+{
+int id;
+String name;
+candidate (int id, String name)
+  {
+this.id = id;
+this.name = name;
+} 
+} 
+
+class student extends candidate
+{
+int marks;
+student (int id, String name, int marks)
+  {
+   super (id, name); //parent or super class constructor  
+   this.marks = marks;
+} 
+
+void display ()
+  {
+   System.out.println (id + " " + name + " " + marks)
+} 
+} 
+
+class superDemo
+{
+public static void main (String[]args)
+  {
+   student stud = new student (1001, "Ramu", 97);
+   stud.display ();
+}}
+
+/* Output:
+
+1001 Ramu 97
+
+*/
