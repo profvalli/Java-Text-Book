ac781328c98aaec026de1fa7383920cf1e7fcc27
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-28 07:36:05 +0530

    Create program7.9-cmdLine.java

---
+++ program7.9-cmdLine.java
@@ -0,0 +1,15 @@
+//Program 7.9 Program to print the command line arguments.
+
+//cmdLine.java
+
+//Click here for code
+
+class cmdLine
+{
+  public static void main (String args[])
+  {
+	for (int i = 0; i < args.length; i++)
+	  System.out.println ("args[" + i + ":" + args[i]);
+  }
+
+}
