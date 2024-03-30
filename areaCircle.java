cc96ef73fd915caa41b9cc49847e3c9f1ec84cc7
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-30 06:31:42 +0530

    Create areaCircle.java

---
+++ areaCircle.java
@@ -0,0 +1,38 @@
+//areaCircle.java
+
+import java.io.*;
+
+public class areacircle
+{
+
+  public static void main (String args[])
+  {
+	float r = 0.0f;
+	float pi = 3.14f;
+	float area = 0.0f;
+	  try
+	{
+	  Console in = System.console ();
+	  String fmt = "%1$4s %2$10s %3$10s%n";
+      // Read line
+	  String radstring = in.readLine (fmt, "Enter", "radius : ", " ");
+	    r = Float.valueOf (radstring);
+
+	}
+
+	catch (Exception e)
+	{
+
+	  System.out.println ("error" + e);
+
+	  System.exit (1);
+
+	}
+
+	area = pi * (float) Math.pow (r, 2.0f);
+
+	System.out.println ("Aread:" + area);
+
+  }
+
+}
