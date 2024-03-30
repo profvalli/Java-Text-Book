82702ca3b251324c35adb8c4a93c3c4b255cea61
Prof V Valli Kumari <45869826+profvalli@users.noreply.github.com>
2024-03-30 07:25:40 +0530

    Create program9.4throwsExample.java

---
+++ program9.4throwsExample.java
@@ -0,0 +1,38 @@
+//Program 9.4 Write a program to demonstrate throws
+
+//throws1.java
+
+class throwsExample{
+
+static void th() throws NullPointerException{
+
+System.out.println("inside th()");
+
+throw new NullPointerException("demon");
+
+}
+
+public static void main(String args[]){
+
+try{
+
+th();
+
+}
+
+catch(NullPointerException e){
+
+System.out.println("caught"+e);
+
+}
+
+}
+
+}
+
+/* 
+Output:
+inside th()
+caughtjava.lang.NullPointerException: demon
+
+*/
